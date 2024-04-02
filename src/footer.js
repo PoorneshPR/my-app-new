f17fcdd3cd6f8176fca0d61efdb9a6487bc7a32d
PoorneshPR <poorneshpr@gmail.com>
2024-04-02 12:38:40 +0530

    sample updation

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     const year = new Date();
   return (
-    <footer> Copyright &copy; { year.getFullYear()} </footer>
+    <footer> Copyright  &copy; { year.getFullYear()} </footer>
   )
 }
 export default Footer;
